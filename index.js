f29d3520d0500b073f4af3428b492ce25da23899
priy-am <priyam2101753@gmail.com>
2024-07-09 00:54:02 +0530

    page

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoutes)
 app.use("/api/v1/tweet", tweetRoutes)
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen(process.env.PORT,()=>{
     console.log(`Example app listening on port ${process.env.port}`)
