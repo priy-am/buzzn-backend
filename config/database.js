7a6f061a1a525b0e26eb34e54601c9714aa3e780
priy-am <priyam2101753@gmail.com>
2024-07-09 20:11:11 +0530

    mongo

--- config/database.js
+++ config/database.js
@@ -5,7 +5,8 @@ dotenv.config({
     path:"./.env"
 })
 // const monoUrl = process.env.MONGO_URL;
-const monoUrl = "mongodb://localhost:27017/twitterClone"
+// const monoUrl = "mongodb://localhost:27017/twitterClone"
+const monoUrl = "mongodb+srv://codepriyam:Priyam@8800@cluster-url/test?retryWrites=true&w=majority"
 
 const databaseConnection = ()=>{
     mongoose.connect(monoUrl).then(()=>{
